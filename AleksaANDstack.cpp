950e4bec1d90eb3b4b4357251fe7934d34457f34
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-08 09:26:54 +0600

    Add files via upload

---
+++ AleksaANDstack.cpp
@@ -0,0 +1,60 @@
+/*---ॐ हरे कृष्णा ॐ---*/
+//Author:-Sourav Dev
+
+#include<iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define umap unordered_map
+#define uset unordered_set
+#define mk make_pair
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define PHI 1.618033988749895 // Golden Ratio
+#define PSI -0.618033988749895 // Conjugate of Golden Ratio
+#define read(type) readInt<type>()
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int a=n;
+        if(a%2==0) a+=1;
+        for(int i=0;i<n;i++)
+        {
+            cout<<a<<" ";
+            a+=2;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
