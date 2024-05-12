b10fd9e4fc7228ce655b7ec029471a42c4845b47
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-12 09:23:58 +0600

    Add files via upload

---
+++ basicdatatype.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int a;
+    long long int b;
+    char ch;
+    float c;
+    double d;
+
+    cin >> a >> b >> ch >> c >> d;
+    cout << a << endl << b << endl << ch << endl << c << endl << d ;
+return 0;
+}
---
+++ bearANDbig_brother.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,n=0;
+    cin>>a>>b;
+ 
+    while(a<=b)
+    
+    {
+        a=3*a;
+        b=2*b;
+        n++;
+    }
+    cout<<n<<endl;
+}
