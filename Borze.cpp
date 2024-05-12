b9702f3219dc2fee03491fc8f467479ac104c119
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-12 09:25:00 +0600

    Add files via upload

---
+++ Borze.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+    int n=str.size();
+
+    for(int i=0;i<n;i++)
+     {
+        if (str[i]=='.')
+        {
+            cout<<0;
+        }
+        else if (str[i]=='-' && str[i+1]=='.')
+        {
+            cout<<1;
+            i++;
+        }
+         else if(str[i]=='-' && str[i+1]=='-')
+        {
+            cout<<2;
+            i++;
+        }
+    }
+
+return 0;
+}
