73f4c1188f044dcecfd75f395251f3294565ce5c
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-19 22:37:23 +0600

    Add files via upload

---
+++ Elephant.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0;
+    cin>>n;
+
+    while(n%5==0)
+    {
+        count++;
+
+    if(n%5 > 1)
+        count++;
+
+
+
+    }
+
+    cout<<count<<endl;
+
+
+}
+
---
+++ easyfibonacci.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,first=0,second=1;
+    cin>>n;
+
+    for(int i=0;i<=n;i++)
+    {
+        cout<<first+second;
+    }
+}
