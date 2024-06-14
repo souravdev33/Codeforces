0025a7b7573766de09ab55be027df3b6ead265a8
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-06-14 22:24:42 +0600

    Add files via upload

---
+++ pascal_triangle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long line;
+	cin>>line;
+	int value;
+	for(int i=0;i<line;i++)
+        {
+		value = 1;
+		for(int z =0;z<=i;z++)
+		{
+		    cout<<value<<" ";
+			value =value*(i-z)/(z+1);
+
+	}
+		cout<<endl;
+
+}
+return 0;
+}
