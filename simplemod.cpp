c6bb26fe0f432ee97aa2678589e472ec40ffbf03
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-06-19 09:45:39 +0600

    Add files via upload

---
+++ simplemod.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+
+  while(t--)
+    {
+    int n;
+    cin>>n;
+
+    vector<int>a(n);
+    for (int i=0; i<n;i++)
+    {
+      cin>>a[i];
+    }
+
+    bool gd=true;
+
+    if (n == 2) {
+      gd = true;
+    } else {
+      int target =a[0]+a[1];
+
+      for (int i=1;i<n-1;i++)
+      {
+        if(a[i]+a[i+1]!=target)
+        {
+          gd=false;
+          break;
+        }
+      }
+    }
+
+    if (gd)
+    {
+      cout<<"Yes"<<endl;
+    }
+    else
+    {
+      cout<<"No"<<endl;
+    }
+  }
+
+return 0;
+}
